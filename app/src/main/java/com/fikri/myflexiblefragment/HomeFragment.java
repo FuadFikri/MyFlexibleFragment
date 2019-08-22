ec60627bd038e25eea8bf2bc3b34b983d2885947
Fikri <fuadfikri01@gmail.com>
2019-08-23 06:42:37 +0700

    berpindah fragment

---
+++ app/src/main/java/com/fikri/myflexiblefragment/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.fikri.myflexiblefragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+//     layout interface didefinisikan dan ditransformasikan dari layout berupa file xml ke dalam obyek view dengan menggunakan metode inflate().
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCategory = view.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_category) {
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                CategoryFragment mCategoryFragment = new CategoryFragment();
+                FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+                mFragmentTransaction.replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName());
+
+//              addToBackStack =  obyek fragment yang saat ini kita ciptakan masuk ke dalam sebuah fragment stack.  Nantinya ketika kita tekan tombol back, ia akan pop-out keluar dari stack dan menampilkan obyek fragment sebelumnya, HomeFragment.
+//                mFragmentTransaction.addToBackStack(null);
+                mFragmentTransaction.commit();
+            }
+        }
+    }
+}
